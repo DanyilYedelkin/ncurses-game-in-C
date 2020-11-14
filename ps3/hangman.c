7267c64e097d456d3845ab51345b3ef7f2132ba1
Danyil Yedelkin <danyil.yedelkin@student.tuke.sk>
2020-11-14 20:35:15 +0100

    Oh yeah, Sir, it's my game

--- ps3/hangman.c
+++ ps3/hangman.c
@@ -92,11 +92,11 @@ void get_available_letters(const char letters_guessed[], char available_letters[
 }
 void hangman(const char secret[]){
     char letters_guessed[27], alphabet[27] = "abcdefghijklmnopqrstuvwxyz", new_letter[30], guessed_word[30];
-    int congratulation = 0, probel = 0, mistake_answer = 0, cuckold = 0, correct_answer = 0;    
+    int congratulation = 0, probel = 0, mistake_answer = 0, cuckold = 0, correct_answer = 0, mis = 0;    
 
-    printf("Wellcome to the game, Hangman!");
+    printf("Welcome to the game, Hangman!");
     printf("\n");
-    printf("I'm thinking of a word that is %ld letters long.", strlen(secret));
+    printf("I am thinking of a word that is %ld letters long.", strlen(secret));
     printf("\n-------------\n");
 
     for(int attempt = 8; attempt != 0;){
@@ -105,16 +105,16 @@ void hangman(const char secret[]){
         printf("Please guess a letter: ");
         scanf("%s", new_letter);
 
-        correct_answer = 0, mistake_answer = 0, cuckold = 0;
+        correct_answer = 0, mistake_answer = 0, cuckold = 0, mis = 0;
 
         for(int i = 0; i < strlen(new_letter); i++){
             new_letter[i] = tolower(new_letter[i]);
         }
 
-        for (int i = 0; new_letter[i]; i++)
+        for(int i = 0; i < strlen(new_letter); i++)
         {
-            if (new_letter[i] < 97){ 
-                printf("Oops! '%c' is not a valid letter: ", (char)new_letter[i]);
+            if(new_letter[i] < 97){ 
+                printf("Oops! '%c' is not a valid letter:", (char)new_letter[i]);
                 for (int i=0; new_letter[i]; i++){ 
                     if (new_letter[i] < 123 && new_letter[i] > 96 && strchr(letters_guessed, new_letter[i]) == NULL){
                         letters_guessed[probel++] = new_letter[i];
@@ -123,7 +123,7 @@ void hangman(const char secret[]){
  
                 get_guessed_word(secret, letters_guessed, guessed_word);
                 get_available_letters(letters_guessed, alphabet);
-                for (int i=0; guessed_word[i]; i++){
+                for (int i = 0; guessed_word[i]; i++){
                     printf("%2c", guessed_word[i]);
                 } 
                 printf("\n-------------\n");
@@ -131,8 +131,8 @@ void hangman(const char secret[]){
                 continue;
 
             } else if(new_letter[i] > 122){
-                printf("Oops! '%c' is not a valid letter: ", (char)new_letter[i]);
-                for (int i=0; new_letter[i]; i++){ 
+                printf("Oops! '%c' is not a valid letter:", (char)new_letter[i]);
+                for (int i = 0; new_letter[i]; i++){ 
                     if (new_letter[i] < 123 && new_letter[i] > 96 && strchr(letters_guessed, new_letter[i]) == NULL){
                         letters_guessed[probel++] = new_letter[i];
                     }
@@ -140,23 +140,25 @@ void hangman(const char secret[]){
  
                 get_guessed_word(secret, letters_guessed, guessed_word);
                 get_available_letters(letters_guessed, alphabet);
-                for (int i=0; guessed_word[i]; i++){
+                for (int i = 0; guessed_word[i]; i++){
                     printf("%2c", guessed_word[i]);
                 } 
                 printf("\n-------------\n");
                 cuckold++;
                 continue;
             }
-            if (strchr(letters_guessed, new_letter[i]) != NULL){
-                mistake_answer++;  
-            } else if ( strchr(secret, new_letter[i]) != NULL){
+            if(strchr(letters_guessed, new_letter[i]) != NULL){
+                mistake_answer++; 
+                mis++;
+            } else if(strchr(secret, new_letter[i]) != NULL){
                 correct_answer++;  
             }
+            mistake_answer++;
         }
 
-        if (correct_answer == 0 && cuckold == 0 && mistake_answer == 0){
-            printf("Oops! That letter is not in my word: ");
-            for (int i=0; new_letter[i]; i++){  
+        if(correct_answer == 0 && cuckold == 0 && mistake_answer == 1){
+            printf("Oops! That letter is not in my word:");
+            for(int i = 0; new_letter[i]; i++){  
                 if (new_letter[i] < 123 && new_letter[i] > 96 && strchr(letters_guessed, new_letter[i]) == NULL){
                     letters_guessed[probel++] = new_letter[i];
                 }
@@ -165,14 +167,28 @@ void hangman(const char secret[]){
             get_guessed_word(secret, letters_guessed, guessed_word);
             get_available_letters(letters_guessed, alphabet);
  
-            for (int i=0; guessed_word[i]; i++){
+            for(int i=0; guessed_word[i]; i++){
                 printf("%2c", guessed_word[i]);
             } 
             printf("\n-------------\n");
             attempt--;
-        } else if(mistake_answer > 0){
-            printf("Oops! You've already guessed that letter: ");
-            for (int i=0; new_letter[i]; i++){ 
+        } else if(mistake_answer == 2 && mis == 1){
+            printf("Oops! You've already guessed that letter:");
+            for(int i = 0; new_letter[i]; i++){ 
+                if(new_letter[i] < 123 && new_letter[i] > 96 && strchr(letters_guessed, new_letter[i]) == NULL){
+                    letters_guessed[probel++] = new_letter[i];
+                }
+            }
+ 
+            get_guessed_word(secret, letters_guessed, guessed_word);
+            get_available_letters(letters_guessed, alphabet);
+            for(int i = 0; guessed_word[i]; i++){
+                printf("%2c", guessed_word[i]);
+            }
+            printf("\n-------------\n");
+        } else if(correct_answer == 1 && cuckold == 0){
+            printf("Good guess:");
+            for(int i = 0; new_letter[i]; i++){  
                 if (new_letter[i] < 123 && new_letter[i] > 96 && strchr(letters_guessed, new_letter[i]) == NULL){
                     letters_guessed[probel++] = new_letter[i];
                 }
@@ -180,37 +196,28 @@ void hangman(const char secret[]){
  
             get_guessed_word(secret, letters_guessed, guessed_word);
             get_available_letters(letters_guessed, alphabet);
-            for (int i=0; guessed_word[i]; i++){
+            for(int i = 0; guessed_word[i]; i++){
                 printf("%2c", guessed_word[i]);
             }
             printf("\n-------------\n");
-        } else if(correct_answer > 0 && cuckold == 0){
-            printf("Good guess: ");
-            for (int i=0; new_letter[i]; i++){  
-                if (new_letter[i] < 123 && new_letter[i] > 96 && strchr(letters_guessed, new_letter[i]) == NULL){
-                    letters_guessed[probel++] = new_letter[i];
-                }
-            }
- 
-            get_guessed_word(secret, letters_guessed, guessed_word);
-            get_available_letters(letters_guessed, alphabet);
-            for (int i=0; guessed_word[i]; i++){
-                printf("%2c", guessed_word[i]);
-            }
-            printf("\n-------------\n");
-            if (is_word_guessed(secret, letters_guessed)){
+            if(is_word_guessed(secret, letters_guessed)){
                 congratulation = 1;
             }
         }
-        if (is_word_guessed(secret, letters_guessed)){
+        for(int i = 0; new_letter[i]; i++){  
+            if (new_letter[i] < 123 && new_letter[i] > 96 && strchr(letters_guessed, new_letter[i]) == NULL){
+                letters_guessed[probel++] = new_letter[i];
+            }
+        }
+        if(is_word_guessed(secret, letters_guessed)){
             congratulation = 1;
         }
-        if (congratulation > 0){
+        if(congratulation > 0){
             printf("Congratulations, you won!\n");
             break;
         }
     }
-    if (congratulation != 1){
+    if(congratulation != 1){
         printf("Sorry, you ran out of guesses. The word was %s.\n", secret);
     }
 }
