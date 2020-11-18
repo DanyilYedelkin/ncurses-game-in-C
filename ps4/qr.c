5b404dbd89ca0081366a10f1ee4129249371b196
Danyil Yedelkin <danyil.yedelkin@student.tuke.sk>
2020-11-19 00:44:26 +0100

    Pridany QR code

--- ps4/qr.c
+++ ps4/qr.c
@@ -11,13 +11,14 @@ void bytes_to_blocks(const int cols, const int offset, bool blocks[offset*8][col
 void blocks_to_bytes(const int cols, const int offset, bool blocks[offset*8][cols], const int rows, bool bytes[rows][8]);
 
 int main(){
-    bool bits1[8];
+    /*bool bits1[8];
     encode_char('A', bits1);
     for(int i = 0; i < 8; i++){
         printf("%d", bits1[i]);
     }
     printf("\n");
-    bool bits2[8] = {0,1,0,0,0,0,0,1};
+    printf("----------------\n");
+    bool bits2[8] = {0,0,1,0,0,0,0,1};
     printf("%c\n", decode_byte(bits2));
     printf("\n--------------\n");
 
@@ -47,18 +48,20 @@ int main(){
     char string[7];
     decode_bytes(7, bytes2, string);
     printf("%s\n", string);
-    printf("\n-------------------\n");
+    printf("\n-------------------\n");*/
 
     //==============================
 
-    int length = 4+1, cols = 3, offset = 2;
-    bool bytes11[4+1][8] = {
-        {0,1,0,0,0,0,0,1},
-        {0,1,1,0,1,0,0,0},
-        {0,1,1,0,1,1,1,1},
-        {0,1,1,0,1,0,1,0},
-        {0,0,0,0,0,0,0,0}
-    };
+   /* int length = 4+1, cols = 2, offset = 4;
+    bool bytes11[7][8] = {
+	{0, 1, 1, 1, 1, 0, 1, 0},
+	{0, 1, 1, 0, 1, 0, 0, 1},
+	{0, 1, 1, 1, 0, 0, 1, 1},
+	{0, 1, 1, 0, 1, 0, 1, 1},
+	{0, 1, 1, 0, 0, 0, 0, 1},
+	{0, 1, 1, 1, 0, 1, 0, 0},
+	{0, 0, 0, 0, 0, 0, 0, 0}
+};
     bool blocks11[offset*8][cols];
     bytes_to_blocks(cols, offset, blocks11, length, bytes11);
     for(int j = 0; j < offset*8; j++){
@@ -69,35 +72,52 @@ int main(){
         if(j % 8 == 7){
             printf("\n");
         }
-    }
+    }*/
 
     printf("----------------\n");
-    bool blocks22[2*8][3] = {
-        {0,0,0},
-        {1,1,1},
-        {0,1,1},
-        {0,0,0},
-        {0,1,1},
-        {0,0,1},
-        {0,0,1},
-        {1,0,1},
-        {0,0,0},
-        {1,0,0},
-        {1,0,0},
-        {0,0,0},
-        {1,0,0},
-        {0,0,0},
-        {1,0,0},
-        {0,0,0}
-    };
-    bool bytes22[length][8];
-    blocks_to_bytes(3, 2, blocks22, length, bytes22);
-    for(int j = 0; j < length; j++){
-        for(int i = 0; i < 8; i++){
-            printf("%d", bytes22[j][i]);
-        }
-        printf("\n");
+    //int length = 7;
+    bool blocks2[4*8][2] = {
+	{0, 0},
+	{1, 1},
+	{1, 1},
+	{1, 0},
+	{1, 1},
+	{0, 0},
+	{1, 0},
+	{0, 1},
+	{0, 0},
+	{1, 1},
+	{1, 1},
+	{1, 0},
+	{0, 1},
+	{0, 0},
+	{1, 1},
+	{1, 1},
+	{0, 0},
+	{1, 1},
+	{1, 1},
+	{0, 1},
+	{0, 0},
+	{0, 1},
+	{0, 0},
+	{1, 0},
+	{0, 0},
+	{0, 0},
+	{0, 0},
+	{0, 0},
+	{0, 0},
+	{0, 0},
+	{0, 0},
+	{0, 0}
+};
+bool bytes2[7][8];
+blocks_to_bytes(2, 4*8, blocks2, 7, bytes2);
+for(int j = 0; j < 7; j++){
+    for(int i = 0; i < 8; i++){
+        printf("%d", bytes2[j][i]);
     }
+    printf("\n");
+}
 
     return 0;
 }
@@ -122,13 +142,13 @@ void encode_char(const char character, bool bits[8]){
     }
 }
 char decode_byte(const bool bits[8]){
-    int sum;
+    int sum = 0;;
     int value = 128;
     for(int i = 0; i < 8; i++){
         sum += (value * bits[i]);
         value /= 2;
     }
-    char symbol = sum + 1;
+    char symbol = sum;
     return symbol;
 }
 void encode_string(const char string[], bool bytes[strlen(string)+1][8]){
@@ -174,30 +194,76 @@ void decode_bytes(const int rows, bool bytes[rows][8], char string[rows]){
     }
 }
 void bytes_to_blocks(const int cols, const int offset, bool blocks[offset*8][cols], const int rows, bool bytes[rows][8]){
-    for(int i = 0; i < offset*8; i++){
+    for(int i = 0; i < 8; i++){
         for(int j = 0; j < cols; j++){
             blocks[i][j] = bytes[j][i];
         }
     }
     if(offset > 1){
-        for(int i = offset*4; i < offset*8; i++){
-            for(int j = 0; j < cols; j++){
-                blocks[i][j] = bytes[cols - 1 + j][i];
-            }
+    for(int i = 8; i < 16; i++){
+        for(int j = 0; j < cols; j++){
+            blocks[i][j] = bytes[cols - 1 + j][i];
         }
+    }
+    }
+    
+    if(offset > 2){
+    for(int i = 16; i < 24; i++){
+        for(int j = 0; j < cols; j++){
+            blocks[i][j] = bytes[cols + j][i];
+        }
+    }
+    }
+    
+    if(offset > 3){
+    for(int i = 24; i < 32; i++){
+        for(int j = 1; j < cols; j++){
+            blocks[i][j] = bytes[cols + j][i];
+        }
+    }
     }
 }
+
 void blocks_to_bytes(const int cols, const int offset, bool blocks[offset*8][cols], const int rows, bool bytes[rows][8]){
-    for(int i = 0; i < offset*8; i++){
-        for(int j = 0; j < cols; j++){
-            bytes[j][i] = blocks[i][j];
+    //for(int offset_counter = 0; offset_counter < offset; offset_counter++){
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < 8; j++){
+                bytes[i][j] = blocks[j][i];
+            }
+        }
+    //}
+    for(int i = 0; i < cols; i++){
+        for(int j = 0; j < 8; j++){
+            bytes[i][j] = blocks[j][i];
         }
     }
-    if(offset > 1){
-        for(int i = offset*4; i < offset*8; i++){
-            for(int j = 0; j < cols; j++){
-                bytes[cols - 1 + j][i] = blocks[i][j];
-            }
+    int value3 = 0;
+    for(int i = 0; i < cols; i++){
+        for(int j = 0; j < 8; j++){
+            bytes[2][j] = blocks[value3++ - 1][2];  
+        }
+    }
+
+    for(int i = 0; i < cols; i++){
+        for(int j = 0; j < 8; j++){
+            bytes[3][j] = blocks[j+8][i];  
+        }
+    }
+    int value2 = 8;
+    for(int i = 0; i < cols; i++){
+        for(int j = 0; j < 8; j++){
+            bytes[4][j] = blocks[value2++ - 1][2];  
+        }
+    }
+    int value = 8;
+    for(int i = 0; i < cols; i++){
+        for(int j = 0; j < 8; j++){
+            bytes[5][j] = blocks[value++][i]; 
+        }
+    }
+    for(int i = 0; i < cols; i++){
+        for(int j = 0; j < 8; j++){
+            bytes[6][j] = blocks[0][0]; 
         }
     }
 }
