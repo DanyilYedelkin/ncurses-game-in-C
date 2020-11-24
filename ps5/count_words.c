a9e02bc25c9dc8ee7fc2c1f575ae6562e13fb8c0
Danyil Yedelkin <danyil.yedelkin@student.tuke.sk>
2020-11-24 21:29:19 +0100

    Oh yeah, Sir, it's my ps5

---
+++ ps5/count_words.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]){
+    if(argc != 2){
+        perror("Wrong number of parameters.\n");  //printf("");
+        exit(EXIT_FAILURE);  //exit (0);
+    }
+    FILE *fp = fopen(argv[1], "r");
+    
+    if(fp == NULL){
+        perror("Error opening file.\n");  //printf("");
+        exit(EXIT_FAILURE);  //exit(0);
+    }
+
+    int counter = 48;
+    char ch = fgetc(fp);
+    while(ch != EOF){
+        if(ch == 'a' || ch == 'A'){
+            ch = fgetc(fp);
+            if(ch == 'n' || ch == 'N'){
+                ch = fgetc(fp);
+                if(ch == 'a' || ch == 'A'){
+                    ch = fgetc(fp);
+                    if(ch == 'n' || ch == 'N'){
+                        ch = fgetc(fp);
+                        if(ch == 'a' || ch == 'A'){
+                            ch = fgetc(fp);
+                            if(ch == 's' || ch == 'S'){
+                                counter++;
+                            }
+                        } 
+                    }
+                }
+            }
+        }
+        ch = fgetc(fp);
+    }
+    fclose(fp);
+    
+    char zero = 48;
+    char one = 49;
+    char two = 50;
+    char three = 51;
+    char four = 52;
+    char five = 53;
+    char six = 54;
+    char seven = 55;
+    char eight = 56;
+    char nine = 57;
+    
+    fp = fopen(argv[1], "w");
+    if(counter < 58){
+        fputc(counter, fp);
+    } else{
+        if(counter == 58){
+            fputc(one, fp);
+            fputc(zero, fp);
+        }
+        if(counter == 59){
+            fputc(one, fp);
+            fputc(one, fp);
+        }
+        if(counter == 60){
+            fputc(one, fp);
+            fputc(two, fp);
+        }
+        if(counter == 61){
+            fputc(one, fp);
+            fputc(three, fp);
+        }
+        if(counter == 62){
+            fputc(one, fp);
+            fputc(four, fp);
+        }
+        if(counter == 63){
+            fputc(one, fp);
+            fputc(five, fp);
+        }
+        if(counter == 64){
+            fputc(one, fp);
+            fputc(six, fp);
+        }
+        if(counter == 65){
+            fputc(one, fp);
+            fputc(seven, fp);
+        }
+        if(counter == 66){
+            fputc(one, fp);
+            fputc(eight, fp);
+        }
+        if(counter == 67){
+            fputc(one, fp);
+            fputc(nine, fp);
+        }
+    }
+
+    fclose(fp);
+
+    return EXIT_SUCCESS;
+}
---
+++ ps5/every_second.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]){
+    if(argc != 3){
+        perror("Wrong number of parameters.\n");  //printf("");
+        exit(EXIT_FAILURE);  //exit (0);
+    }
+    FILE *fp = fopen(argv[1], "r");
+    
+    if(fp == NULL){
+        perror("Error opening file.\n");  //printf("");
+        exit(EXIT_FAILURE);  //exit(0);
+    }
+    FILE *fs = fopen(argv[2], "w");
+    
+    if(fs == NULL){
+        perror("Error opening file.\n");  //printf("");
+        exit(EXIT_FAILURE);  //exit(0);
+    }
+
+    char ch = fgetc(fp);
+    while(ch != EOF){
+        if(ch == 'S'){
+            ch = fgetc(fp);
+            if(ch == 'T'){
+                ch = fgetc(fp);
+                if(ch == 'A'){
+                    ch = fgetc(fp);
+                    if(ch == 'R'){
+                        ch = fgetc(fp);
+                        if(ch == 'T'){
+                            ch = fgetc(fp);
+                            char chr = ch;
+                            char r = ch;
+                            while(ch != EOF){                                
+                                ch = fgetc(fp);
+                                
+                                if(ch == 'S'){
+                                    ch = fgetc(fp);
+                                    if(ch == 'T'){
+                                        ch = fgetc(fp);
+                                        if(ch == 'O'){
+                                            ch = fgetc(fp);
+                                            if(ch == 'P'){
+                                                break;
+                                            }
+                                        }
+                                    }
+                                }
+                                if(ch == ' '){
+                                    ch = fgetc(fp);
+                                    chr = ch;
+                                    r = ch;
+                                    fputc(chr, fs);
+
+                                    while(ch != ' '){
+                                        ch = fgetc(fp);
+                                        chr = ch;
+                                        r = ch;
+                                        if(r == ' '){
+                                            r = fgetc(fp);
+                                            if(r == 'S'){
+                                                break;
+                                            }
+                                        }
+                                        fputc(chr, fs);
+                                    }
+                                }
+                            }
+                        } 
+                    }
+                }
+            }
+        }
+        ch = fgetc(fp);
+    }
+
+    fclose(fp);
+    fclose(fs);
+
+    return EXIT_SUCCESS;
+}
