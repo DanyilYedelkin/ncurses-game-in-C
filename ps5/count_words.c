c6ade68a416e3a509416781a3c1f2deb8e19349b
Danyil Yedelkin <danyil.yedelkin@student.tuke.sk>
2020-11-25 23:21:36 +0100

    Oh yeah, Sir, look at my works

--- ps5/count_words.c
+++ ps5/count_words.c
@@ -3,15 +3,14 @@
 
 int main(int argc, char* argv[]){
     if(argc != 2){
-        printf("Wrong number of parameters.\n");  //printf("");
-        return 1;  //exit (0);
+        printf("Wrong number of parameters.\n");
+        return 1;
     }
     FILE *fp = fopen(argv[1], "r");
     
     if(fp == NULL){
-        printf("Error opening file.\n");  //printf("");
-        fclose(fp);
-        return 1;  //exit(0);
+        printf("Error opening file.\n");
+        return 1;
     }
 
     int counter = 48;
--- ps5/every_second.c
+++ ps5/every_second.c
@@ -3,79 +3,106 @@
 
 int main(int argc, char* argv[]){
     if(argc != 3){
-        printf("Wrong number of parameters.\n");  //printf("");
-        return 1;  //exit (0);
+        printf("Wrong number of parameters.\n");
+        return 1;
     }
     FILE *fp = fopen(argv[1], "r");
     
     if(fp == NULL){
-        printf("Error opening file.\n");  //printf("");
+        printf("Error opening file.\n");
         fclose(fp);
-        return 1;  //exit(0);
+        return 1;
     }
     FILE *fs = fopen(argv[2], "w");
     
     if(fs == NULL){
-        printf("Error opening file.\n");  //printf("");
+        printf("Error opening file.\n"); 
         fclose(fs);
-        return 1;  //exit(0);
+        return 1;
+    }
+    
+    char input[1000];
+    char between[1000];
+    int input_length = 0;
+    int between_length = 0;
+    
+    for(int i = 0; i < 1000; i++){
+        input[i] = fgetc(fp);
+        input_length++;
+        if(input[i] == EOF){
+            input[i] = '\0';
+            break;
+        }
     }
 
-    char ch = fgetc(fp);
-    while(ch != EOF){
-        if(ch == 'S'){
-            ch = fgetc(fp);
-            if(ch == 'T'){
-                ch = fgetc(fp);
-                if(ch == 'A'){
-                    ch = fgetc(fp);
-                    if(ch == 'R'){
-                        ch = fgetc(fp);
-                        if(ch == 'T'){
-                            ch = fgetc(fp);
-                            char chr = ch;
-                            char r = ch;
-                            while(ch != EOF){                                
-                                ch = fgetc(fp);
-                                
-                                if(ch == 'S'){
-                                    ch = fgetc(fp);
-                                    if(ch == 'T'){
-                                        ch = fgetc(fp);
-                                        if(ch == 'O'){
-                                            ch = fgetc(fp);
-                                            if(ch == 'P'){
-                                                break;
-                                            }
-                                        }
-                                    }
-                                }
-                                if(ch == ' '){
-                                    ch = fgetc(fp);
-                                    chr = ch;
-                                    r = ch;
-                                    fputc(chr, fs);
-
-                                    while(ch != ' '){
-                                        ch = fgetc(fp);
-                                        chr = ch;
-                                        r = ch;
-                                        if(r == ' '){
-                                            r = fgetc(fp);
-                                            if(r == 'S'){
-                                                break;
-                                            }
-                                        }
-                                        fputc(chr, fs);
-                                    }
-                                }
-                            }
-                        } 
-                    }
+    for(int i = 0; i < input_length; i++){
+        if(input[i] == 'S' && input[i+1] == 'T' && input[i+2] == 'A' && input[i+3] == 'R' && input[i+4] == 'T'){
+            for(int j = i+6; j < input_length; j++){
+                if(input[j] == 'S' && input[j+1] == 'T' && input[j+2] == 'O' && input[j+3] == 'P'){
+                    between[between_length - 1] = '\0';
+                    break;
                 }
+                between[between_length] = input[j];
+                between_length++;
             }
         }
-        ch = fgetc(fp);
+    }
+    
+    int i = 0;
+    int value = 0;
+    int k = 0;
+    char output[1000];
+
+    while(between[i] != '\0'){
+        while(between[i] != ' '){
+            if(between[i+1] != '\0'){
+                i++;
+                k++;
+            } else{
+                break;
+            }
+        }
+        i++;
+        k++;
+        while(between[i] != ' '){
+            if(between[i+1] != '\0'){
+                output[value] = between[i];
+                i++;
+                k++;
+                value++;
+            } else{
+                break;
+            }
+        }
+        if(between[i+1] != '\0'){
+            if(between[k] == ' '){
+                k++;
+                while(between[k] != ' '){
+                    k++;
+                }
+                if(between[k] == '\0'){
+                    output[value - 1] = '\0';
+                    break;
+                }
+            }
+            output[value] = between[i];
+            i++;
+            k++;
+            value++;
+        }
+    }
+
+    int j = 0;
+    while(output[j] != '\0'){
+        if(output[j] == ' '){
+            if(output[j+1] != '\0'){
+                fputc(output[j], fs);
+            }
+        } else{
+            fputc(output[j], fs);
+        }
+        
+        j++;
     }
 
     fclose(fp);
